f80f4d9a8ecf3b425c9a6d7e19d653d0326f265c
Palisar <83838480+Palisar@users.noreply.github.com>
2021-09-17 17:08:39 +0100

    added explanier comment

--- BeehiveManagementSystem/Bee.cs
+++ BeehiveManagementSystem/Bee.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace BeehiveManagementSystem
 {
+    /// <summary>
+    /// The bee class is at the top of our hierarchy which makes it more abstrack
+    /// the higher we go in a class hierarchy the more abstract the classes become and the lower we go, the more concrete they are
+    /// </summary>
     abstract class Bee : IWorker
     {
         public string Job { get; private set;}
